2632289eb68ff1dda015eefe69f1f03b6e85185c
Jamie Lentin <jm@lentin.co.uk>
2017-11-20 17:10:46 +0000

    install.sh: Fix nginx syntax errors

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ server {
         alias "${SERVER_CERT_PATH}/acme-challenge/";
     }
 
-    return 301 https://$server_name$request_uri;
+    return 301 https://\$server_name\$request_uri;
 }
 
 server {
@@ -97,9 +97,9 @@ server {
     listen      443 ssl;
     server_name ${SERVER_NAME};
 
-    ssl_certificate      ${SERVER_CERT_PATH}/${SERVER_NAME}/fullchain.pem;
-    ssl_certificate_key  ${SERVER_CERT_PATH}/${SERVER_NAME}/privkey.pem;
-    ssl_trusted_certificate ${SERVER_CERT_PATH}/${SERVER_NAME}/fullchain.pem;
+    ssl_certificate      ${SERVER_CERT_PATH}/certs/${SERVER_NAME}/fullchain.pem;
+    ssl_certificate_key  ${SERVER_CERT_PATH}/certs/${SERVER_NAME}/privkey.pem;
+    ssl_trusted_certificate ${SERVER_CERT_PATH}/certs/${SERVER_NAME}/fullchain.pem;
     ssl_dhparam ${SERVER_CERT_PATH}/dhparam.pem;
 
     # https://mozilla.github.io/server-side-tls/ssl-config-generator/
@@ -107,7 +107,7 @@ server {
     ssl_session_cache shared:SSL:50m;
     # intermediate configuration. tweak to your needs.
     ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-R
+    ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
     ssl_prefer_server_ciphers on;
 
 EOF
