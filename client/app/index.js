5872bad14bf4d46cd5c661ac435f1dbe7dd0ae1a
Jamie Lentin <jm@lentin.co.uk>
2017-11-27 17:36:31 +0000

    client/templates.js: Move table templates into own lib

--- client/app/index.js
+++ client/app/index.js
@@ -6,96 +6,12 @@ var XLSX = require('xlsx');
 var FileSaver = require('file-saver');
 var jQuery = require('jquery/dist/jquery.slim.js');
 var get_dimension = require('./dimensions.js').get_dimension;
+var table_templates = require('./templates.js').table_templates;
 jQuery = require('select2')(jQuery);
-
-var tableTemplate = [
-    {
-        name: "constants",
-        title: "Constants",
-        orientation: "vertical",
-        fields: {type: "list", values: [
-            "Data source type", // i.e. where the data came from TODO: vocab?
-            "Data source description",
-            "Duration t",  // i.e. number columns for templateCatch
-            "Average catch over time t", // Mean of catch
-            "Depletion over time t",
-            "M",
-            "FMSY/M",
-            "BMSY/B0",
-            "MSY",
-            "BMSY",
-            "Length at 50% maturity",
-            "Length at 95% maturity",
-            "Length at first capture",
-            "Length at full selection",
-            "Current stock depletion",
-            "Current stock abundance",
-            "Von Bertalanffy K parameter",
-            "Von Bertalanffy Linf parameter",
-            "Von Bertalanffy t0 parameter",
-            "Length-weight parameter a",
-            "Length-weight parameter b",
-            "Steepness",
-            "Maximum age",
-            "CV Catch",
-            "CV Depletion over time t",
-            "CV Average catch over time t",
-            "CV Abundance index",
-            "CV M",
-            "CV FMSY/M",
-            "CV BMSY/B0",
-            "CV current stock depletion",
-            "CV current stock abundance",
-            "CV von B. K parameter",
-            "CV von B. Linf parameter",
-            "CV von B. t0 parameter",
-            "CV Length at 50% maturity",
-            "CV Length at first capture",
-            "CV Length at full selection",
-            "CV Length-weight parameter a",
-            "CV Length-weight parameter b",
-            "CV Steepness",
-            "Sigma length composition",
-            "Units",
-            "Reference OFL",
-            "Reference OFL type",
-            "MPrec",
-            "LHYear",
-        ]},
-        values: {type: "list", values: ["Value"]},
-        params: {rowHeaderWidth: 270},
-    },
-    {
-        name: "catch",
-        title: "Catch data",
-        orientation: "vertical",
-        fields: {type: "list", values: [
-            "Catch",
-            "Abundance index",
-        ]},
-        values: {type: 'year', min: 2000, max: 2010},
-        params: {rowHeaderWidth: 170},
-    },
-    {
-        name: "caa",
-        title: "Catch at age",
-        orientation: "horizontal",
-        fields: {type: "bins", count: 10},
-        values: {type: "year", min: 2000, max: 2010},
-    },
-    {
-        name: "cal",
-        title: "Catch at length",
-        orientation: "horizontal",
-        fields: {type: "bins", count: 10},
-        values: {type: "year", min: 2000, max: 2010, initial: ["Min Length"]},
-        params: {rowHeaderWidth: 100},
-    },
-];
 
 var tbl = document.getElementById("tbl");
 
-var hots = tableTemplate.map(function (tmpl) {
+var hots = table_templates.dlmtool.map(function (tmpl) {
     var hot, hotParams, cols, rows,
         el = document.createElement("div");
 
@@ -158,6 +74,7 @@ document.querySelector("#options button[name=save]").addEventListener('click', f
         var i,
             data = hot.getData(),
             tmpl = tableTemplate[tableIndex],
+            tmpl = table_templates.dlmtool[tableIndex],
             rowHeaders = hot.getRowHeader();
 
         // Add column header
@@ -207,7 +124,7 @@ document.querySelector("#options button[name=export]").addEventListener('click',
     hots.map(function (hot, tableIndex) {
         var i,
             data = hot.getData(),
-            tmpl = tableTemplate[tableIndex],
+            tmpl = table_templates.dlmtool[tableIndex],
             rowHeaders = hot.getRowHeader();
 
         // Add column header
