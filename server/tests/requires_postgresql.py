6e3f6936f728f7412d9a3a3cc54db9857218d1d0
Jamie Lentin <jm@lentin.co.uk>
2018-05-03 16:24:39 +0000

    server/db: Add some tests for db module

---
+++ server/tests/requires_postgresql.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import testing.postgresql
+
+
+def runSqlScript(postgresql, script):
+    return subprocess.run((
+        'psql', '-b',
+        '-f', script,
+        '-h', 'localhost',
+        '-p', str(postgresql.settings['port']),
+        '-U', 'postgres', '-w',
+        'test',
+    ), check=True, stdout=subprocess.PIPE)
+
+
+def initDatabase(postgresql):
+    dir = '../schema'
+    for s in sorted(os.listdir(dir)):
+        if not s.endswith('.sql'):
+            continue
+        runSqlScript(postgresql, os.path.join(dir, s))
+
+
+Postgresql = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
+
+
+class RequiresPostgresql():
+    def setUp(self):
+        super(RequiresPostgresql, self).setUp()
+
+        self.postgresql = testing.postgresql.Postgresql()
+        initDatabase(self.postgresql)
+        self.conn = psycopg2.connect(**self.postgresql.dsn())
+
+    def cursor(self):
+        return self.conn.cursor(cursor_factory=RealDictCursor)
+
+    def tearDown(self):
+        self.conn.close()
+        self.postgresql.stop()
+
+        super(RequiresPostgresql, self).tearDown()
--- server/tests/test_db.py
+++ server/tests/test_db.py
@@ -1,6 +1,63 @@
 import unittest
 
+import ffdb.db as db
+from tests.requires_postgresql import RequiresPostgresql
 
-class TestDB(unittest.TestCase):
-    def test_moo(self):
-        pass  # TODO:
+
+class TestDB(RequiresPostgresql, unittest.TestCase):
+    def test_documents(self):
+        """
+        We can store, retrieve and list documents
+        """
+        with self.cursor() as cursor:
+            # Nothing there yet
+            self.assertEqual(db.list_documents(cursor, 'tmpl0'), [])
+            self.assertEqual(db.list_documents(cursor, 'tmpl1'), [])
+
+            # Store something, there is
+            self.assertEqual(db.store_document(cursor, 'tmpl0', 'doc0', dict(cows=['daisy'])), dict(
+                template_name='tmpl0',
+                document_name='doc0',
+                version=1,
+                content=dict(cows=['daisy']),
+            ))
+            self.assertEqual(db.list_documents(cursor, 'tmpl0'), [dict(document_name='doc0', latest=1)])
+            self.assertEqual(db.list_documents(cursor, 'tmpl1'), [])
+
+            # Can store new versions
+            self.assertEqual(db.store_document(cursor, 'tmpl0', 'doc0', dict(cows=['daisy', 'freda'])), dict(
+                template_name='tmpl0',
+                document_name='doc0',
+                version=2,
+                content=dict(cows=['daisy', 'freda']),
+            ))
+            self.assertEqual(db.list_documents(cursor, 'tmpl0'), [dict(document_name='doc0', latest=2)])
+            self.assertEqual(db.list_documents(cursor, 'tmpl1'), [])
+
+            # ...or new documents
+            self.assertEqual(db.store_document(cursor, 'tmpl0', 'doc1', dict(pigs=['george'])), dict(
+                template_name='tmpl0',
+                document_name='doc1',
+                version=1,
+                content=dict(pigs=['george']),
+            ))
+            self.assertEqual(db.list_documents(cursor, 'tmpl0'), [
+                dict(document_name='doc0', latest=2),
+                dict(document_name='doc1', latest=1),
+            ])
+            self.assertEqual(db.list_documents(cursor, 'tmpl1'), [])
+
+            # ... with different templates
+            self.assertEqual(db.store_document(cursor, 'tmpl1', 'doc0', dict(pigs=['emily'])), dict(
+                template_name='tmpl1',
+                document_name='doc0',
+                version=1,
+                content=dict(pigs=['emily']),
+            ))
+            self.assertEqual(db.list_documents(cursor, 'tmpl0'), [
+                dict(document_name='doc0', latest=2),
+                dict(document_name='doc1', latest=1),
+            ])
+            self.assertEqual(db.list_documents(cursor, 'tmpl1'), [
+                dict(document_name='doc0', latest=1),
+            ])
