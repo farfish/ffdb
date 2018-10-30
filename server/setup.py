aed643fd260ef5985abdb81eadadec5c74b74792
Jamie Lentin <jm@lentin.co.uk>
2018-10-30 10:08:18 +0000

    server/requirements: Overhaul requirements mechanism, upgrade Flask

--- server/setup.py
+++ server/setup.py
@@ -1,8 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+requires = [
+    'Flask',
+    'uWSGI',
+    'psycopg2',
+]
+
+tests_require = [
+    'pytest',
+    'pytest-cov',
+    'testing.postgresql',
+]
 
 setup(
   name="ffdb",
-  packages=['ffdb'],
+  description='FFDB web API',
+  author='Jamie Lentin',
+  author_email='jamie.lentin@shuttlethread.com',
+  url='https://github.com/farfish/ffdb',
+  packages=find_packages(),
+  install_requires=requires,
+  extras_require=dict(
+    testing=tests_require,
+  ),
   entry_points={
         'console_scripts': [
         ],
