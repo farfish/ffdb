27d0678163611ec00608fe17bb7d34b0e785e8b5
Jamie Lentin <jm@lentin.co.uk>
2019-10-24 15:30:13 +0100

    schema/updates: Only add author column if table exists in first place

--- schema/000-updates.sql
+++ schema/000-updates.sql
@@ -5,13 +5,18 @@ BEGIN;
 DO
 $do$
 BEGIN
-   IF NOT EXISTS (SELECT *
-                FROM information_schema.columns
+   IF EXISTS (SELECT 1
+                FROM information_schema.tables
                WHERE table_schema = 'public'
-                 AND table_name = 'document'
-                 AND column_name = 'author') THEN
-       ALTER TABLE document
-           ADD COLUMN author TEXT;
+                 AND table_name = 'document') THEN
+       IF NOT EXISTS (SELECT *
+                    FROM information_schema.columns
+                   WHERE table_schema = 'public'
+                     AND table_name = 'document'
+                     AND column_name = 'author') THEN
+           ALTER TABLE document
+               ADD COLUMN author TEXT;
+       END IF;
    END IF;
 END
 $do$;
