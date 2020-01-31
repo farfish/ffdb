020ce94f8717f6996fe76569b6849510157e6079
Jamie Lentin <jm@lentin.co.uk>
2020-01-31 10:09:43 +0000

    schema: Input log here makes no sense, it should be per-document

--- schema/000-updates.sql
+++ schema/000-updates.sql
@@ -41,5 +41,26 @@ BEGIN
 END
 $do$;
 
+-- Remove input_log column from table_hashes, move to document.
+DO
+$do$
+BEGIN
+   IF EXISTS (SELECT 1
+                FROM information_schema.tables
+               WHERE table_schema = 'public'
+                 AND table_name = 'document') THEN
+       IF NOT EXISTS (SELECT *
+                    FROM information_schema.columns
+                   WHERE table_schema = 'public'
+                     AND table_name = 'document'
+                     AND column_name = 'input_hashes') THEN
+           ALTER TABLE model_output
+               DROP COLUMN input_log;
+           ALTER TABLE document
+               ADD COLUMN model_input_log TEXT NULL;
+       END IF;
+   END IF;
+END
+$do$;
 
 COMMIT;
--- schema/002-model_output.sql
+++ schema/002-model_output.sql
@@ -7,13 +7,11 @@ CREATE TABLE IF NOT EXISTS model_output (
     PRIMARY KEY (model_name, input_hash),
 
     input_rdata              BYTEA,
-    input_log                TEXT,
     output_path              TEXT NULL,
     output_log               TEXT
 );
 COMMENT ON TABLE model_output IS 'model input --> model output';
 COMMENT ON COLUMN model_output.input_hash IS 'Unique hash of input_rdata object';
-COMMENT ON COLUMN model_output.input_log IS 'Output/warnings whilst generating input';
 COMMENT ON COLUMN model_output.output_path IS 'Absolute path to output file';
 COMMENT ON COLUMN model_output.output_log IS 'Output/warnings whilst generating output';
 
