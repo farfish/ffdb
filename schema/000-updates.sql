2f9d219ce873c14a1434be10f73f4da12090fcec
Jamie Lentin <jm@lentin.co.uk>
2020-02-10 18:06:42 +0000

    schema: Fix upgrade step
    mnodel_input_log wasn't ever used, condigitons flat-out broken.

--- schema/000-updates.sql
+++ schema/000-updates.sql
@@ -48,16 +48,14 @@ BEGIN
    IF EXISTS (SELECT 1
                 FROM information_schema.tables
                WHERE table_schema = 'public'
-                 AND table_name = 'document') THEN
-       IF NOT EXISTS (SELECT *
+                 AND table_name = 'model_output') THEN
+       IF EXISTS (SELECT *
                     FROM information_schema.columns
                    WHERE table_schema = 'public'
-                     AND table_name = 'document'
-                     AND column_name = 'input_hashes') THEN
+                     AND table_name = 'model_output'
+                     AND column_name = 'input_log') THEN
            ALTER TABLE model_output
                DROP COLUMN input_log;
-           ALTER TABLE document
-               ADD COLUMN model_input_log TEXT NULL;
        END IF;
    END IF;
 END
